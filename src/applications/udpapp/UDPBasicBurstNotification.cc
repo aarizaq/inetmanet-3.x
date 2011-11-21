56597144c4fc80502bc7cbbbd32e6fff64abf369
unknown <alfonso@.(none)>
2011-11-21 12:54:15 +0100

    Include new source with signal notification, this source can change the destination address if change the number of destination (delete nodes or new nodes)

---
+++ src/applications/udpapp/UDPBasicBurstNotification.cc
@@ -0,0 +1,198 @@
+//
+// Copyright (C) 2000 Institut fuer Telematik, Universitaet Karlsruhe
+// Copyright (C) 2007 Universidad de Malaga
+// Copyright (C) 2011 Zoltan Bojthe
+// Copyright (C) 2011 Alfonso Ariza, Universidad de Malaga
+//
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License
+// as published by the Free Software Foundation; either version 2
+// of the License, or (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+//
+
+
+#include "UDPBasicBurstNotification.h"
+
+#include "UDPControlInfo_m.h"
+#include "IPvXAddressResolver.h"
+#include "InterfaceTable.h"
+#include "InterfaceTableAccess.h"
+
+
+Define_Module(UDPBasicBurstNotification);
+
+UDPBasicBurstNotification::UDPBasicBurstNotification()
+{
+    addressModule = NULL;
+}
+
+UDPBasicBurstNotification::~UDPBasicBurstNotification()
+{
+    if (addressModule)
+        delete addressModule;
+}
+
+void UDPBasicBurstNotification::initialize(int stage)
+{
+    // because of IPvXAddressResolver, we need to wait until interfaces are registered,
+    // address auto-assignment takes place etc.
+    if (stage != 3)
+        return;
+
+    counter = 0;
+    numSent = 0;
+    numReceived = 0;
+    numDeleted = 0;
+    numDuplicated = 0;
+
+    delayLimit = par("delayLimit");
+    simtime_t startTime = par("startTime");
+    stopTime = par("stopTime");
+
+    messageLengthPar = &par("messageLength");
+    burstDurationPar = &par("burstDuration");
+    sleepDurationPar = &par("sleepDuration");
+    sendIntervalPar = &par("sendInterval");
+    nextSleep = startTime;
+    nextBurst = startTime;
+    nextPkt = startTime;
+
+    destAddrRNG = par("destAddrRNG");
+    const char *addrModeStr = par("chooseDestAddrMode").stringValue();
+    int addrMode = cEnum::get("ChooseDestAddrMode")->lookup(addrModeStr);
+    if (addrMode == -1)
+        throw cRuntimeError(this, "Invalid chooseDestAddrMode: '%s'", addrModeStr);
+    chooseDestAddrMode = (ChooseDestAddrMode)addrMode;
+
+    WATCH(numSent);
+    WATCH(numReceived);
+    WATCH(numDeleted);
+    WATCH(numDuplicated);
+
+    localPort = par("localPort");
+    destPort = par("destPort");
+
+    socket.setOutputGate(gate("udpOut"));
+    socket.bind(localPort);
+    if (par("setBroadcast").boolValue())
+        socket.setBroadcast(true);
+
+    if (strcmp(par("outputInterface").stringValue(),"") != 0)
+    {
+        IInterfaceTable* ift = InterfaceTableAccess().get();
+        InterfaceEntry *ie = ift->getInterfaceByName(par("outputInterface").stringValue());
+        if (ie == NULL)
+            throw cRuntimeError(this, "Invalid output interface name : %s",par("outputInterface").stringValue());
+        outputInterface = ie->getInterfaceId();
+    }
+
+    if (strcmp(par("outputInterfaceMulticastBroadcast").stringValue(),"") != 0)
+    {
+        IInterfaceTable* ift = InterfaceTableAccess().get();
+        InterfaceEntry *ie = ift->getInterfaceByName(par("outputInterfaceMulticastBroadcast").stringValue());
+        if (ie == NULL)
+            throw cRuntimeError(this, "Invalid output interface name : %s",par("outputInterfaceMulticastBroadcast").stringValue());
+        outputInterfaceMulticastBroadcast = ie->getInterfaceId();
+    }
+
+    addressModule = new AddressModule();
+    //addressModule->initModule(par("chooseNewIfDeleted").boolValue());
+    addressModule->initModule(true);
+
+
+    isSource = (addressModule->getNumAddress() > 0);
+    if (isSource)
+    {
+        if (chooseDestAddrMode == ONCE)
+            destAddr = chooseDestAddr();
+
+        activeBurst = true;
+
+        timerNext = new cMessage("UDPBasicBurstTimer");
+        scheduleAt(startTime, timerNext);
+        /*
+        if (par("chooseNewIfDeleted").boolValue())
+        {
+            simulation.getSystemModule()->subscribe(POST_MODEL_CHANGE, this);
+        }
+        */
+    }
+
+    sentPkSignal = registerSignal("sentPk");
+    rcvdPkSignal = registerSignal("rcvdPk");
+    outOfOrderPkSignal = registerSignal("outOfOrderPk");
+    dropPkSignal = registerSignal("dropPk");
+}
+
+IPvXAddress UDPBasicBurstNotification::chooseDestAddr()
+{
+    return addressModule->choseNewAddress();
+}
+
+
+void UDPBasicBurstNotification::generateBurst()
+{
+    simtime_t now = simTime();
+
+    if (nextPkt < now)
+        nextPkt = now;
+
+    double sendInterval = sendIntervalPar->doubleValue();
+    if (sendInterval <= 0.0)
+        throw cRuntimeError(this, "The sendInterval parameter must be bigger than 0");
+    nextPkt += sendInterval;
+
+    if (activeBurst && nextBurst <= now) // new burst
+    {
+        double burstDuration = burstDurationPar->doubleValue();
+        if (burstDuration < 0.0)
+            throw cRuntimeError(this, "The burstDuration parameter mustn't be smaller than 0");
+        double sleepDuration = sleepDurationPar->doubleValue();
+
+        if (burstDuration == 0.0)
+            activeBurst = false;
+        else
+        {
+            if (sleepDuration < 0.0)
+                throw cRuntimeError(this, "The sleepDuration parameter mustn't be smaller than 0");
+            nextSleep = now + burstDuration;
+            nextBurst = nextSleep + sleepDuration;
+        }
+
+        if (chooseDestAddrMode == PER_BURST)
+            chooseDestAddr();
+    }
+
+    if (chooseDestAddrMode == PER_SEND)
+        chooseDestAddr();
+
+    destAddr = addressModule->getAddress();
+    cPacket *payload = createPacket();
+    payload->setTimestamp();
+    emit(sentPkSignal, payload);
+
+    // Check address type
+    if (outputInterfaceMulticastBroadcast != -1 && (destAddr.isMulticast() || (!destAddr.isIPv6() && destAddr.get4() == IPv4Address::ALLONES_ADDRESS)))
+        socket.sendTo(payload, destAddr, destPort,outputInterfaceMulticastBroadcast);
+    else
+        socket.sendTo(payload, destAddr, destPort,outputInterface);
+
+    numSent++;
+
+    // Next timer
+    if (activeBurst && nextPkt >= nextSleep)
+        nextPkt = nextBurst;
+
+    scheduleAt(nextPkt, timerNext);
+}
+
+
---
+++ src/applications/util/AddressModule.h
@@ -0,0 +1,44 @@
+//
+// Copyright (C) Alfonso Ariza 2011 Universidad de Malaga
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Lesser General Public License for more details.
+// 
+// You should have received a copy of the GNU Lesser General Public License
+// along with this program.  If not, see http://www.gnu.org/licenses/.
+// 
+
+#ifndef AddressModule_H_
+#define AddressModule_H_
+#include <vector>
+#include <map>
+
+#include "INETDefs.h"
+#include "IPvXAddress.h"
+
+class AddressModule : public cNoncopyableOwnedObject,cListener
+{
+    protected:
+        bool emitSignal;
+        std::vector<IPvXAddress> destAddresses;
+        IPvXAddress chosedAddresses;
+        static simsignal_t changeAddressSignal;
+        virtual void receiveSignal(cComponent *src, simsignal_t id, cObject *obj);
+
+    public:
+        virtual void initModule(bool mode);
+        virtual IPvXAddress getAddress(int val = -1);
+        unsigned int getNumAddress() const {return destAddresses.size();}
+        virtual IPvXAddress choseNewAddress();
+        AddressModule();
+        virtual ~AddressModule();
+};
+
+#endif /* AddressModule_H_ */
