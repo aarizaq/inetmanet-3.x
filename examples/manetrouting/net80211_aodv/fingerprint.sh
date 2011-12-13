5694271f21679111fcc5484887d6c97237b44037
unknown <alfonso@pc1te.(none)>
2011-12-13 13:23:46 +0100

    Upload the fingerprint test

---
+++ examples/manetrouting/net80211_aodv/fingerprint.sh
@@ -0,0 +1,7 @@
+rm x__*.out
+
+for ((i=0;i<100;i=i+1)) do
+echo -n "run $i: " >>x__$i.out
+opp_run -n ../../../src:../../../examples:../../../tests/fingerprint -l ../../../src/inet -u Cmdenv -f omnetpp.ini -c General -r 0 --sim-time-limit=60s --fingerprint=c3fa-4898 --cpu-time-limit=40s --vector-recording=false --scalar-recording=false | grep Fingerprint >>x__$i.out
+sleep 1
+done
