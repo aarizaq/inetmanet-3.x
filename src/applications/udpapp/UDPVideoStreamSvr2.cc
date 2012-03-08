a9e9d8f1f0b0f7652ef5e6cd700c273a761b3d0e
alfonso <alfonso@alfonso-System-Product-Name.(none)>
2012-03-09 00:05:04 +0100

    Basic vide model with reintent

---
+++ src/applications/udpapp/UDPVideoStreamSvr2.cc
@@ -0,0 +1,157 @@
+//
+// Copyright (C) 2005 Andras Varga
+//
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU Lesser General Public License
+// as published by the Free Software Foundation; either version 2
+// of the License, or (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with this program; if not, see <http://www.gnu.org/licenses/>.
+//
+
+
+//
+// based on the video streaming app of the similar name by Johnny Lai
+//
+
+
+#include "UDPVideoStreamSvr2.h"
+
+#include "UDPControlInfo_m.h"
+#include "UDPVideoData_m.h"
+
+
+Define_Module(UDPVideoStreamSvr2);
+
+simsignal_t UDPVideoStreamSvr2::reqStreamBytesSignal = SIMSIGNAL_NULL;
+simsignal_t UDPVideoStreamSvr2::sentPkSignal = SIMSIGNAL_NULL;
+
+inline std::ostream& operator<<(std::ostream& out, const UDPVideoStreamSvr2::VideoStreamData& d)
+{
+    out << "client=" << d.clientAddr << ":" << d.clientPort
+        << "  size=" << d.videoSize << "  pksent=" << d.numPkSent << "  bytesleft=" << d.bytesLeft;
+    return out;
+}
+
+UDPVideoStreamSvr2::UDPVideoStreamSvr2()
+{
+}
+
+UDPVideoStreamSvr2::~UDPVideoStreamSvr2()
+{
+    for (unsigned int i=0; i < streamVector.size(); i++)
+        delete streamVector[i];
+}
+
+void UDPVideoStreamSvr2::initialize()
+{
+    sendInterval = &par("sendInterval");
+    packetLen = &par("packetLen");
+    videoSize = &par("videoSize");
+    localPort = par("localPort");
+
+    // statistics
+    numStreams = 0;
+    numPkSent = 0;
+    reqStreamBytesSignal = registerSignal("reqStreamBytes");
+    sentPkSignal = registerSignal("sentPk");
+
+    WATCH_PTRVECTOR(streamVector);
+
+    socket.setOutputGate(gate("udpOut"));
+    socket.bind(localPort);
+}
+
+void UDPVideoStreamSvr2::finish()
+{
+}
+
+void UDPVideoStreamSvr2::handleMessage(cMessage *msg)
+{
+    if (msg->isSelfMessage())
+    {
+        // timer for a particular video stream expired, send packet
+        sendStreamData(msg);
+    }
+    else if (msg->getKind() == UDP_I_DATA)
+    {
+        // start streaming
+        processStreamRequest(msg);
+    }
+    else if (msg->getKind() == UDP_I_ERROR)
+    {
+        EV << "Ignoring UDP error report\n";
+        delete msg;
+    }
+    else
+    {
+        error("Unrecognized message (%s)%s", msg->getClassName(), msg->getName());
+    }
+}
+
+void UDPVideoStreamSvr2::processStreamRequest(cMessage *msg)
+{
+    // register video stream...
+    UDPDataIndication *ctrl = check_and_cast<UDPDataIndication *>(msg->getControlInfo());
+
+    VideoStreamData *d = new VideoStreamData;
+    d->clientAddr = ctrl->getSrcAddr();
+    d->clientPort = ctrl->getSrcPort();
+    d->videoSize = (*videoSize);
+    d->bytesLeft = d->videoSize;
+    d->numPkSent = 0;
+    streamVector.push_back(d);
+    delete msg;
+
+    cMessage *timer = new cMessage("VideoStreamTmr");
+    timer->setContextPointer(d);
+
+    // ... then transmit first packet right away
+    sendStreamData(timer);
+
+    numStreams++;
+    emit(reqStreamBytesSignal, d->videoSize);
+}
+
+void UDPVideoStreamSvr2::sendStreamData(cMessage *timer)
+{
+    VideoStreamData *d = (VideoStreamData *) timer->getContextPointer();
+
+    // generate and send a packet
+    UDPVideoDataPacket *pkt = new UDPVideoDataPacket("VideoStrmPk");
+    long pktLen = packetLen->longValue();
+
+    if (pktLen > d->bytesLeft)
+        pktLen = d->bytesLeft;
+
+    pkt->setVideoSize(d->videoSize);
+    pkt->setBytesLeft(d->bytesLeft);
+    pkt->setNumPkSent(d->numPkSent);
+
+    pkt->setByteLength(pktLen);
+    emit(sentPkSignal, pkt);
+    socket.sendTo(pkt, d->clientAddr, d->clientPort);
+
+    d->bytesLeft -= pktLen;
+    d->numPkSent++;
+    numPkSent++;
+
+    // reschedule timer if there's bytes left to send
+    if (d->bytesLeft != 0)
+    {
+        simtime_t interval = (*sendInterval);
+        scheduleAt(simTime()+interval, timer);
+    }
+    else
+    {
+        delete timer;
+        // TBD find VideoStreamData in streamVector and delete it
+    }
+}
+
---
+++ src/applications/udpapp/UDPVideoStreamSvr2.h
@@ -0,0 +1,93 @@
+//
+// Copyright (C) 2005 Andras Varga
+//
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU Lesser General Public License
+// as published by the Free Software Foundation; either version 2
+// of the License, or (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with this program; if not, see <http://www.gnu.org/licenses/>.
+//
+
+
+//
+// based on the video streaming app of the similar name by Johnny Lai
+//
+
+#ifndef __INET_UDPVIDEOSTREAMSVR2_H
+#define __INET_UDPVIDEOSTREAMSVR2_H
+
+
+#include <vector>
+
+#include "INETDefs.h"
+#include "UDPSocket.h"
+
+
+/**
+ * Stream VBR video streams to clients.
+ *
+ * Cooperates with UDPVideoStreamCli. UDPVideoStreamCli requests a stream
+ * and UDPVideoStreamSvr starts streaming to them. Capable of handling
+ * streaming to multiple clients.
+ */
+class INET_API UDPVideoStreamSvr2 : public cSimpleModule
+{
+  public:
+    /**
+     * Stores information on a video stream
+     */
+    struct VideoStreamData
+    {
+        IPvXAddress clientAddr;   ///< client address
+        int clientPort;           ///< client UDP port
+        long videoSize;           ///< total size of video
+        long bytesLeft;           ///< bytes left to transmit
+        long numPkSent;           ///< number of packets sent
+    };
+
+  protected:
+    typedef std::vector<VideoStreamData *> VideoStreamVector;
+    VideoStreamVector streamVector;
+    UDPSocket socket;
+
+    // module parameters
+    int localPort;
+    cPar *sendInterval;
+    cPar *packetLen;
+    cPar *videoSize;
+
+    // statistics
+    unsigned int numStreams;  // number of video streams served
+    unsigned long numPkSent;  // total number of packets sent
+    static simsignal_t reqStreamBytesSignal;  // length of video streams served
+    static simsignal_t sentPkSignal;
+
+  protected:
+    // process stream request from client
+    virtual void processStreamRequest(cMessage *msg);
+
+    // send a packet of the given video stream
+    virtual void sendStreamData(cMessage *timer);
+
+  public:
+    UDPVideoStreamSvr2();
+    virtual ~UDPVideoStreamSvr2();
+
+  protected:
+    ///@name Overridden cSimpleModule functions
+    //@{
+    virtual void initialize();
+    virtual void finish();
+    virtual void handleMessage(cMessage* msg);
+    //@}
+};
+
+#endif
+
