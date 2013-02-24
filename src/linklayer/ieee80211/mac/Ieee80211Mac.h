1b059e692296b858226f087b431879bc0bd5f0de
Alfonso <aarizaq_m@hotmail.com>
2013-02-24 19:42:35 +0100

    include method that publish mac status

--- src/linklayer/ieee80211/mac/Ieee80211Mac.h
+++ src/linklayer/ieee80211/mac/Ieee80211Mac.h
@@ -314,6 +314,9 @@ class INET_API Ieee80211Mac : public WirelessMacBase, public INotifiable
         EDCA,
     };
   protected:
+    // if true MAC sublayer store the frames instead of the management sublayer
+    bool queueMode;
+
     Mode mode;
 
     /** Sequence number to be assigned to the next frame */
@@ -687,6 +690,11 @@ class INET_API Ieee80211Mac : public WirelessMacBase, public INotifiable
         nb->fireChangeNotification(category, pkt);
         pkt->setKind(tempKind);
     }
+  public:
+    virtual void setQueueModeTrue() {queueMode = true;}
+    virtual void setQueueModeFalse() {queueMode = false;}
+    virtual State getState() {return static_cast<State>(fsm.getState());}
+    virtual unsigned int getQueueSize() {return transmissionQueueSize();}
 };
 
 #endif
