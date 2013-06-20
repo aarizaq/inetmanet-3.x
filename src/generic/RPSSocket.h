fc4db0ea7ad88313ca60edeade4aa459d196ce65
alfonso <aarizaq_m@hotmail.com>
2013-06-20 09:20:04 +0200

    Inclue (c) and license headers

--- src/generic/RPSSocket.h
+++ src/generic/RPSSocket.h
@@ -1,3 +1,19 @@
+/*
+  *
+  * copyright   (C) 2012 Communication Networks Institute (CNI - Prof. Dr.-Ing. Christian Wietfeld)
+  *                  at Technische Universitaet Dortmund, Germany
+  *                  http://www.kn.e-technik.tu-dortmund.de/
+  *
+  *
+  *              This program is free software; you can redistribute it
+  *              and/or modify it under the terms of the GNU General Public
+  *              License as published by the Free Software Foundation; either
+  *              version 2 of the License, or (at your option) any later
+  *              version.
+  *              For further information see file COPYING
+  *              in the top level directory
+
+  ********************************************************************************/
 #ifndef RPSSOCKET_H_
 #define RPSSOCKET_H_
 
