3f4b3224aab37cf7eb4fa7cd48a9bdedefb3cff7
Alfonso Ariza <aarizaq_m@hotmail.com>
2013-11-14 17:56:02 +0100

    initialize value

--- src/util/GlobalWirelessLinkInspector.cc
+++ src/util/GlobalWirelessLinkInspector.cc
@@ -399,6 +399,7 @@ bool GlobalWirelessLinkInspector::getNumNodes(ManetAddress node, int &cont)
 
 bool GlobalWirelessLinkInspector::areNeighbour(const ManetAddress &node1, const ManetAddress &node2,bool &areNei)
 {
+    areNei = true;
     if (globalLocatorMap == NULL)
         return false;
     LocatorIteartor it1 =  globalLocatorMap->find(node1);
@@ -411,8 +412,6 @@ bool GlobalWirelessLinkInspector::areNeighbour(const ManetAddress &node1, const 
     ManetAddress ap2 = it2->second;
     if (ap1 == ap2)
         areNei = true;
-    else
-        areNei = false;
     return true;
 }
 
